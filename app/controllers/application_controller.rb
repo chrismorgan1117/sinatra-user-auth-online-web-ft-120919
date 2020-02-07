1881f2bf5fc15148d5bbd8008d5c9ffa8ecfac9a
Chris Morgan <cwm111794@gmail.com>
2020-02-06 22:36:54 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
 
   get '/sessions/login' do
 
-    # the line of code below render the view page in app/views/sessions/login.erb
+    # the line of code below renders the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
@@ -45,8 +45,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 end
+ 
